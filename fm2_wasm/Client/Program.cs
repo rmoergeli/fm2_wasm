7d84e26e397b176895667b2982ad8c7cd32cd04c
rmoergeli <rene.moergeli@windowslive.com>
2021-05-09 08:28:51 +0200

    Add project files.

---
+++ fm2_wasm/Client/Program.cs
@@ -0,0 +1,28 @@
+using fm2_wasm.Client.ViewModels;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fm2_wasm.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddScoped<WeatherViewModel>(); // https://stackoverflow.com/questions/67445922/blazor-wasm-viewmodel
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ fm2_wasm/Client/ViewModels/WeatherViewModel.cs
@@ -0,0 +1,58 @@
+﻿using fm2_wasm.Shared;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace fm2_wasm.Client.ViewModels
+{
+    public class WeatherViewModel : BaseViewModel
+    {
+        #region Properties
+        private readonly ILogger<WeatherViewModel> _logger;
+        private HttpClient _http;
+
+        private WeatherForecast[] weatherForecasts;
+        public WeatherForecast[] WeatherForecasts
+        {
+            get => weatherForecasts;
+            private set
+            {
+                weatherForecasts = value;
+                OnPropertyChanged(nameof(WeatherForecasts));
+            }
+        }
+        #endregion
+
+        #region Constructor(s)
+        /// <summary>
+        /// Constructor(s)
+        /// </summary>
+        public WeatherViewModel(ILogger<WeatherViewModel> logger, HttpClient http)
+        {
+            Trace.WriteLine("Constructor...");
+
+            _logger = logger;
+            _http = http;
+
+            _ = RetrieveForecasts();
+        }
+        #endregion
+
+        private async Task RetrieveForecasts()
+        {
+            WeatherForecasts = await RetrieveForecastsAsync();
+        }
+
+        private async Task<WeatherForecast[]> RetrieveForecastsAsync()
+        {
+            WeatherForecast[] _weatherForecast = await _http.GetFromJsonAsync<WeatherForecast[]>("api/SampleData/WeatherForecasts");
+
+            return _weatherForecast;
+        }
+    }
+}
